8306529dcc81ea6f46f4299c332a2b4ade7d82d9
zefrost77 <alexis.fauxbaton@gmail.com>
2022-01-07 17:17:31 +0100

    Création des fichiers Game

---
+++ Game.cpp
@@ -0,0 +1,14 @@
+#include "Game.hpp"
+#include "Inventaire.hpp"
+#include "Equipement.hpp"
+#include "Objet.hpp"
+#include "Personnage.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+Game::Game()
+{
+    
+}
---
+++ Game.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "Inventaire.hpp"
+#include "Equipement.hpp"
+#include "Objet.hpp"
+#include "Personnage.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+class Game
+{
+    public:
+        Game();
+        ~Game();
+        void run();
+
+    private:
+
+};
